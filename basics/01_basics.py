afaf48145f2f6f5b79c4bf0f63f9ccfa9d752af3
Alokdev07 <alokbhuyan162@gmail.com>
2025-08-05 19:08:13 +0530

    This is a new repo about python and i learn about python basics

---
+++ basics/01_basics.py
@@ -0,0 +1,14 @@
+print("hello world")
+
+# escape sequence
+print("i am a good boy\nhere is a second line")
+
+# that's how we write comments in python
+
+'''
+This is how we write multiline comments in python
+'''
+
+print("to use double coat character sequence we use this \"this type\" ")
+
+print("hey this is separator",23, sep="-",end="00\n")
